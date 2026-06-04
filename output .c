6f30f90c5dfd792357337aef804c6066dda202d9
Kesavan-Srinivasan7 <kesavan.srinivasan20@gmail.com>
2026-06-04 17:18:01 +0530

    output and newline

---
+++ output .c
@@ -0,0 +1,9 @@
+//output and newline of c
+ // 1) Print "Hello World!" with a new line
+  // 2) Print "I am learning C.
+# include<stdio.h>
+  int main(){
+    printf("Hello World!\n");
+    printf("I am learning c.");
+    return 0;
+  }
