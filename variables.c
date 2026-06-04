f7ab94282d34f2fd8540a57689f4e2e1ea5c59a8
Kesavan-Srinivasan7 <kesavan.srinivasan20@gmail.com>
2026-06-04 17:31:16 +0530

    deleted

---
+++ variables.c
@@ -0,0 +1,15 @@
+ // Write length here
+  // Write width here
+  // Write area here (length * width)
+
+#include<stdio.h>
+int main(){
+    int length = 2;
+    int width = 2;
+    int area;
+    area = length * width;
+    printf("Length of the square:%d\n",length);
+    printf("widht  of the square:%d\n",width);
+    printf( "The of the square is:%d\n",area);
+    return 0;
+}
